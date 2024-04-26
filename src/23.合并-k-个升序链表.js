f2a202a6288ff23f24cacf9eeff8d3bd13cbf2de
tongtong.ctt <tongtong.ctt@alibaba-inc.com>
2024-04-26 17:17:00 +0800

    feat: 23

--- src/23.合并-k-个升序链表.js
+++ src/23.合并-k-个升序链表.js
@@ -18,7 +18,62 @@ function ListNode(val, next) {
   this.next = next === undefined ? null : next
 }
 
-function priorityQueue() {}
+function createPriorityQueue(sortFunction) {
+  const storage = [null]
+
+  function swap(index1, index2) {
+    const tmp = storage[index1]
+    storage[index1] = storage[index2]
+    storage[index2] = tmp
+  }
+
+  // 下沉
+  function sink(index) {
+    let compareKey = null
+    if (index * 2 + 1 < storage.length) {
+      const isLeft = sortFunction(storage[index * 2], storage[index * 2 + 1])
+      compareKey = isLeft ? index * 2 : index * 2 + 1
+    } else if (index * 2 < storage.length) {
+      compareKey = index * 2
+    }
+    if (compareKey && sortFunction(storage[compareKey], storage[index])) {
+      swap(index, compareKey)
+      sink(compareKey)
+    }
+  }
+
+  // 上浮
+  function swim(index) {
+    if (index === 1) {
+      return
+    }
+    const parentKey = Math.floor(index / 2)
+
+    if (sortFunction(storage[index], storage[parentKey])) {
+      swap(index, parentKey)
+      swim(parentKey)
+    }
+  }
+
+  function push(val) {
+    storage.push(val)
+    swim(storage.length - 1)
+  }
+
+  function pop() {
+    if (storage.length === 1) {
+      return null
+    }
+    swap(1, storage.length - 1)
+    const target = storage.pop()
+    sink(1)
+    return target
+  }
+  return {
+    push,
+    pop,
+  }
+}
 
 /**
  * @param {ListNode[]} lists
@@ -27,19 +82,30 @@ function priorityQueue() {}
 const mergeKLists = function (lists) {
   const resultDummy = new ListNode(-1)
 
-  let links = lists.filter(Boolean)
-  let resultTraverse = resultDummy
-  while (links.some(Boolean)) {
-    const currentValues = links.map((node) => node.val)
-    const minValue = Math.min(...currentValues)
-    const minNodeIndex = links.findIndex((node) => node.val === minValue)
-    resultTraverse.next = links[minNodeIndex]
-
-    resultTraverse = resultTraverse.next
-    links[minNodeIndex] = links[minNodeIndex].next
-
-    links = links.filter(Boolean)
+  const priorityQueue = createPriorityQueue(
+    (node1, node2) => node1.val < node2.val
+  )
+  while (lists.length) {
+    let current = lists.pop()
+    while (current) {
+      priorityQueue.push(current)
+      current = current.next
+    }
   }
+
+  let resultTraverse = resultDummy
+
+  do {
+    const current = priorityQueue.pop()
+    if (!current) {
+      break
+    }
+    resultTraverse.next = current
+    resultTraverse = resultTraverse.next
+  } while (true)
+
+  resultTraverse.next = null
+
   return resultDummy.next
 }
 // @lc code=end
